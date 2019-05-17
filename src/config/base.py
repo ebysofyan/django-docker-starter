098f42b2addb159970690f99d7e9347af9278090
Eby Sofyan Fadly <eby.sofyan@gmail.com>
2019-05-17 16:10:12 +0800

    fix : upgrade django version from 2.1.5 into 2.2.1

--- src/config/base.py
+++ src/config/base.py
@@ -110,3 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/m/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -6,12 +6,23 @@ from ..base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'db-starter',  # set in docker-compose.yml
+#         'PORT': 5432  # default postgres port
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db-starter',  # set in docker-compose.yml
-        'PORT': 5432  # default postgres port
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'abk_db',
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
